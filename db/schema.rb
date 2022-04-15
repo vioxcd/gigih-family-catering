b4b8a0632c9986c96905062ced9e64be5fb602b9
Muhammad Alviand Fredonovalle <46772320+alviandf@users.noreply.github.com>
2022-04-15 07:41:44 +0000

    Migrate Category DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_064320) do
+ActiveRecord::Schema.define(version: 2022_04_15_071941) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
