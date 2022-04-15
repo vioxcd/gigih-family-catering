57d4a63e2db4bb5d69f72bcafd31a0bcea096976
Muhammad Alviand Fredonovalle <46772320+alviandf@users.noreply.github.com>
2022-04-15 10:34:55 +0000

    Add Validation for MenuItem

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,13 @@
 class MenuItem < ApplicationRecord
   has_many :menu_categories
   has_many :categories, through: :menu_categories
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, length: { maximum: 150 }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
