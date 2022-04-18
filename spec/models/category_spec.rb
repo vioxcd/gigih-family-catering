3de8980dd9ec861f5bd8c1dcd9fac8bdac023e89
Muhammad Alviand Fredonovalle <46772320+alviandf@users.noreply.github.com>
2022-04-18 23:41:18 +0000

    add unit test for model Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect(FactoryBot.build(:category)).to be_valid
+  end
+
+  it "is invalid without name" do
+    category = FactoryBot.build(:category, name: nil)
+    
+    expect(category).to be_invalid
+  end
+
+  it "is invalid with duplicate name" do
+    category1 = FactoryBot.create(:category, name: "Main Dish")
+    category2 = FactoryBot.build(:category, name: "Main Dish")
+
+    category2.valid? 
+    
+    expect(category2.errors[:name]).to include("has already been taken")
+  end
+  
 end
